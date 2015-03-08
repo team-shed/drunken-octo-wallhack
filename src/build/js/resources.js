78a769cee9eaeb6b086668a74f697246e6bd3e3b
Jeffrey Kyllo <jkyllo@echospiral.com>
2015-03-08 11:28:04 +0000

    Paying attention to build and adding resources.

---
+++ src/build/js/resources.js
@@ -0,0 +1,15 @@
+                //game resources
+                game.resources = [
+                  /**
+                   * Graphics.
+                   */
+                  // our level tileset
+                  {name: "Wall",  type:"image", src: "data/img/DawnLike_3/Objects/Wall.png"},
+                  {name: "Floor",  type:"image", src: "data/img/DawnLike_3/Objects/Floor.png"},
+                  {name: "player", type:"image", src:"data/img/sprite/gripe_run_right.png"},
+              
+                  /* 
+                   * Maps. 
+                   */
+                  {name: "aroom", type: "tmx", src: "data/map/aroom.tmx"}
+                ];
